99ea9fd40450ca934f22e9cb547b95698d39ab4c
Sepehr1812 <sepehrakhoundi1812@gmail.com>
2020-01-30 02:31:29 +0330

    Config files added.

---
+++ cmake-build-debug/CMakeFiles/xv6_public.dir/DependInfo.cmake
@@ -0,0 +1,72 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/sepehr/Mine/xv6-public/OriginalSchedTest.c" "/home/sepehr/Mine/xv6-public/cmake-build-debug/CMakeFiles/xv6_public.dir/OriginalSchedTest.c.o"
+  "/home/sepehr/Mine/xv6-public/PrioritySchedTest.c" "/home/sepehr/Mine/xv6-public/cmake-build-debug/CMakeFiles/xv6_public.dir/PrioritySchedTest.c.o"
+  "/home/sepehr/Mine/xv6-public/bio.c" "/home/sepehr/Mine/xv6-public/cmake-build-debug/CMakeFiles/xv6_public.dir/bio.c.o"
+  "/home/sepehr/Mine/xv6-public/bootmain.c" "/home/sepehr/Mine/xv6-public/cmake-build-debug/CMakeFiles/xv6_public.dir/bootmain.c.o"
+  "/home/sepehr/Mine/xv6-public/cat.c" "/home/sepehr/Mine/xv6-public/cmake-build-debug/CMakeFiles/xv6_public.dir/cat.c.o"
+  "/home/sepehr/Mine/xv6-public/console.c" "/home/sepehr/Mine/xv6-public/cmake-build-debug/CMakeFiles/xv6_public.dir/console.c.o"
+  "/home/sepehr/Mine/xv6-public/echo.c" "/home/sepehr/Mine/xv6-public/cmake-build-debug/CMakeFiles/xv6_public.dir/echo.c.o"
+  "/home/sepehr/Mine/xv6-public/exec.c" "/home/sepehr/Mine/xv6-public/cmake-build-debug/CMakeFiles/xv6_public.dir/exec.c.o"
+  "/home/sepehr/Mine/xv6-public/file.c" "/home/sepehr/Mine/xv6-public/cmake-build-debug/CMakeFiles/xv6_public.dir/file.c.o"
+  "/home/sepehr/Mine/xv6-public/forktest.c" "/home/sepehr/Mine/xv6-public/cmake-build-debug/CMakeFiles/xv6_public.dir/forktest.c.o"
+  "/home/sepehr/Mine/xv6-public/fs.c" "/home/sepehr/Mine/xv6-public/cmake-build-debug/CMakeFiles/xv6_public.dir/fs.c.o"
+  "/home/sepehr/Mine/xv6-public/getChildrenTest.c" "/home/sepehr/Mine/xv6-public/cmake-build-debug/CMakeFiles/xv6_public.dir/getChildrenTest.c.o"
+  "/home/sepehr/Mine/xv6-public/getCountTest.c" "/home/sepehr/Mine/xv6-public/cmake-build-debug/CMakeFiles/xv6_public.dir/getCountTest.c.o"
+  "/home/sepehr/Mine/xv6-public/grep.c" "/home/sepehr/Mine/xv6-public/cmake-build-debug/CMakeFiles/xv6_public.dir/grep.c.o"
+  "/home/sepehr/Mine/xv6-public/ide.c" "/home/sepehr/Mine/xv6-public/cmake-build-debug/CMakeFiles/xv6_public.dir/ide.c.o"
+  "/home/sepehr/Mine/xv6-public/init.c" "/home/sepehr/Mine/xv6-public/cmake-build-debug/CMakeFiles/xv6_public.dir/init.c.o"
+  "/home/sepehr/Mine/xv6-public/ioapic.c" "/home/sepehr/Mine/xv6-public/cmake-build-debug/CMakeFiles/xv6_public.dir/ioapic.c.o"
+  "/home/sepehr/Mine/xv6-public/kalloc.c" "/home/sepehr/Mine/xv6-public/cmake-build-debug/CMakeFiles/xv6_public.dir/kalloc.c.o"
+  "/home/sepehr/Mine/xv6-public/kbd.c" "/home/sepehr/Mine/xv6-public/cmake-build-debug/CMakeFiles/xv6_public.dir/kbd.c.o"
+  "/home/sepehr/Mine/xv6-public/kill.c" "/home/sepehr/Mine/xv6-public/cmake-build-debug/CMakeFiles/xv6_public.dir/kill.c.o"
+  "/home/sepehr/Mine/xv6-public/lapic.c" "/home/sepehr/Mine/xv6-public/cmake-build-debug/CMakeFiles/xv6_public.dir/lapic.c.o"
+  "/home/sepehr/Mine/xv6-public/ln.c" "/home/sepehr/Mine/xv6-public/cmake-build-debug/CMakeFiles/xv6_public.dir/ln.c.o"
+  "/home/sepehr/Mine/xv6-public/log.c" "/home/sepehr/Mine/xv6-public/cmake-build-debug/CMakeFiles/xv6_public.dir/log.c.o"
+  "/home/sepehr/Mine/xv6-public/ls.c" "/home/sepehr/Mine/xv6-public/cmake-build-debug/CMakeFiles/xv6_public.dir/ls.c.o"
+  "/home/sepehr/Mine/xv6-public/main.c" "/home/sepehr/Mine/xv6-public/cmake-build-debug/CMakeFiles/xv6_public.dir/main.c.o"
+  "/home/sepehr/Mine/xv6-public/memide.c" "/home/sepehr/Mine/xv6-public/cmake-build-debug/CMakeFiles/xv6_public.dir/memide.c.o"
+  "/home/sepehr/Mine/xv6-public/mkdir.c" "/home/sepehr/Mine/xv6-public/cmake-build-debug/CMakeFiles/xv6_public.dir/mkdir.c.o"
+  "/home/sepehr/Mine/xv6-public/mkfs.c" "/home/sepehr/Mine/xv6-public/cmake-build-debug/CMakeFiles/xv6_public.dir/mkfs.c.o"
+  "/home/sepehr/Mine/xv6-public/mp.c" "/home/sepehr/Mine/xv6-public/cmake-build-debug/CMakeFiles/xv6_public.dir/mp.c.o"
+  "/home/sepehr/Mine/xv6-public/picirq.c" "/home/sepehr/Mine/xv6-public/cmake-build-debug/CMakeFiles/xv6_public.dir/picirq.c.o"
+  "/home/sepehr/Mine/xv6-public/pipe.c" "/home/sepehr/Mine/xv6-public/cmake-build-debug/CMakeFiles/xv6_public.dir/pipe.c.o"
+  "/home/sepehr/Mine/xv6-public/policyTest.c" "/home/sepehr/Mine/xv6-public/cmake-build-debug/CMakeFiles/xv6_public.dir/policyTest.c.o"
+  "/home/sepehr/Mine/xv6-public/printf.c" "/home/sepehr/Mine/xv6-public/cmake-build-debug/CMakeFiles/xv6_public.dir/printf.c.o"
+  "/home/sepehr/Mine/xv6-public/proc.c" "/home/sepehr/Mine/xv6-public/cmake-build-debug/CMakeFiles/xv6_public.dir/proc.c.o"
+  "/home/sepehr/Mine/xv6-public/rm.c" "/home/sepehr/Mine/xv6-public/cmake-build-debug/CMakeFiles/xv6_public.dir/rm.c.o"
+  "/home/sepehr/Mine/xv6-public/sh.c" "/home/sepehr/Mine/xv6-public/cmake-build-debug/CMakeFiles/xv6_public.dir/sh.c.o"
+  "/home/sepehr/Mine/xv6-public/sleeplock.c" "/home/sepehr/Mine/xv6-public/cmake-build-debug/CMakeFiles/xv6_public.dir/sleeplock.c.o"
+  "/home/sepehr/Mine/xv6-public/spinlock.c" "/home/sepehr/Mine/xv6-public/cmake-build-debug/CMakeFiles/xv6_public.dir/spinlock.c.o"
+  "/home/sepehr/Mine/xv6-public/stressfs.c" "/home/sepehr/Mine/xv6-public/cmake-build-debug/CMakeFiles/xv6_public.dir/stressfs.c.o"
+  "/home/sepehr/Mine/xv6-public/string.c" "/home/sepehr/Mine/xv6-public/cmake-build-debug/CMakeFiles/xv6_public.dir/string.c.o"
+  "/home/sepehr/Mine/xv6-public/syscall.c" "/home/sepehr/Mine/xv6-public/cmake-build-debug/CMakeFiles/xv6_public.dir/syscall.c.o"
+  "/home/sepehr/Mine/xv6-public/sysfile.c" "/home/sepehr/Mine/xv6-public/cmake-build-debug/CMakeFiles/xv6_public.dir/sysfile.c.o"
+  "/home/sepehr/Mine/xv6-public/sysproc.c" "/home/sepehr/Mine/xv6-public/cmake-build-debug/CMakeFiles/xv6_public.dir/sysproc.c.o"
+  "/home/sepehr/Mine/xv6-public/timeTest.c" "/home/sepehr/Mine/xv6-public/cmake-build-debug/CMakeFiles/xv6_public.dir/timeTest.c.o"
+  "/home/sepehr/Mine/xv6-public/trap.c" "/home/sepehr/Mine/xv6-public/cmake-build-debug/CMakeFiles/xv6_public.dir/trap.c.o"
+  "/home/sepehr/Mine/xv6-public/uart.c" "/home/sepehr/Mine/xv6-public/cmake-build-debug/CMakeFiles/xv6_public.dir/uart.c.o"
+  "/home/sepehr/Mine/xv6-public/ulib.c" "/home/sepehr/Mine/xv6-public/cmake-build-debug/CMakeFiles/xv6_public.dir/ulib.c.o"
+  "/home/sepehr/Mine/xv6-public/umalloc.c" "/home/sepehr/Mine/xv6-public/cmake-build-debug/CMakeFiles/xv6_public.dir/umalloc.c.o"
+  "/home/sepehr/Mine/xv6-public/usertests.c" "/home/sepehr/Mine/xv6-public/cmake-build-debug/CMakeFiles/xv6_public.dir/usertests.c.o"
+  "/home/sepehr/Mine/xv6-public/vm.c" "/home/sepehr/Mine/xv6-public/cmake-build-debug/CMakeFiles/xv6_public.dir/vm.c.o"
+  "/home/sepehr/Mine/xv6-public/wc.c" "/home/sepehr/Mine/xv6-public/cmake-build-debug/CMakeFiles/xv6_public.dir/wc.c.o"
+  "/home/sepehr/Mine/xv6-public/zombie.c" "/home/sepehr/Mine/xv6-public/cmake-build-debug/CMakeFiles/xv6_public.dir/zombie.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../."
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
